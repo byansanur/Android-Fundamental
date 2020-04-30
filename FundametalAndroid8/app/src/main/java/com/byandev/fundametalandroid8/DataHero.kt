a8dd56432dde39cafd840f7bc0f288e4645e88c9
byansanur <ratbyansa25@gmail.com>
2020-04-30 23:36:15 +0700

    recyclerview list

---
+++ FundametalAndroid8/app/src/main/java/com/byandev/fundametalandroid8/DataHero.kt
@@ -0,0 +1,11 @@
+package com.byandev.fundametalandroid8
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Hero(
+    var name: String,
+    var description: String,
+    var photo: String
+) : Parcelable
--- FundametalAndroid8/app/src/main/java/com/byandev/fundametalandroid8/MainActivity.kt
+++ FundametalAndroid8/app/src/main/java/com/byandev/fundametalandroid8/MainActivity.kt
@@ -2,11 +2,65 @@ package com.byandev.fundametalandroid8
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val list = ArrayList<Hero>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rv_heroes.setHasFixedSize(true)
+
+        list.addAll(getListHeroes())
+        showRecyclerList()
+    }
+
+    fun getListHeroes(): ArrayList<Hero> {
+        val dataName = resources.getStringArray(R.array.data_name)
+        val dataDescription = resources.getStringArray(R.array.data_description)
+        val dataPhoto = resources.getStringArray(R.array.data_photo)
+        val listHero = ArrayList<Hero>()
+        for (position in dataName.indices) {
+            val hero = Hero(
+                dataName[position],
+                dataDescription[position],
+                dataPhoto[position]
+            )
+            listHero.add(hero)
+        }
+        return listHero
+    }
+
+    private fun showRecyclerList() {
+        rv_heroes.layoutManager = LinearLayoutManager(this)
+        val listHeroAdapter = ListAdapterHero(list)
+        rv_heroes.adapter = listHeroAdapter
+    }
+
+    // menu on action bar
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_item, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        setMode(item.itemId)
+        return super.onOptionsItemSelected(item)
+    }
+    private fun setMode(selectedMode: Int) {
+        when (selectedMode) {
+            R.id.action_list -> {
+                showRecyclerList()
+            }
+            R.id.action_grid -> {
+            }
+            R.id.action_cardview -> {
+            }
+        }
     }
 }
