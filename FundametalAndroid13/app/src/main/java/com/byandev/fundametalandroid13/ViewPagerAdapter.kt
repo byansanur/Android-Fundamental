d153fc26932ed0915e5b59bc829ef13700b7e8fd
byansanur <ratbyansa25@gmail.com>
2020-05-06 16:15:27 +0700

    Simple tab layout

---
+++ FundametalAndroid13/app/src/main/java/com/byandev/fundametalandroid13/ViewPagerAdapter.kt
@@ -0,0 +1,29 @@
+package com.byandev.fundametalandroid13
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class ViewPagerAdapter (private val context: Context, fm: FragmentManager) : FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+    private val TAB_TITLES = intArrayOf(R.string.tab_text_1, R.string.tab_text_2)
+
+    override fun getItem(position: Int): Fragment {
+        var fragment: Fragment? = null
+        when(position) {
+            0 -> fragment = HomeFragment()
+            1 -> fragment = ProfileFragment()
+        }
+        return fragment as Fragment
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return context.resources.getString(TAB_TITLES[position])
+    }
+
+    override fun getCount(): Int {
+        return 2
+    }
+
+}
