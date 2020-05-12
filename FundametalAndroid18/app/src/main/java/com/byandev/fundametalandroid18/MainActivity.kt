d2739d973402afb9780706cb122d9afd9fcde6eb
byansanur <ratbyansa25@gmail.com>
2020-05-12 23:16:06 +0700

    Broacast dari sms

--- FundametalAndroid18/app/src/main/java/com/byandev/fundametalandroid18/MainActivity.kt
+++ FundametalAndroid18/app/src/main/java/com/byandev/fundametalandroid18/MainActivity.kt
@@ -1,12 +1,43 @@
 package com.byandev.fundametalandroid18
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest.permission.RECEIVE_SMS
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    companion object {
+        private const val SMS_REQUEST_CODE = 101
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_permission.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when {
+            v?.id == R.id.btn_permission -> PermissionManager.check(this, RECEIVE_SMS, SMS_REQUEST_CODE)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == SMS_REQUEST_CODE) {
+            when {
+                grantResults[0] == PackageManager.PERMISSION_GRANTED ->
+                    Toast.makeText(this, "Sms receiver permission diterima", Toast.LENGTH_SHORT).show()
+                else -> Toast.makeText(this,"Sms receiver pemission di tolak",Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
---
+++ FundametalAndroid18/app/src/main/java/com/byandev/fundametalandroid18/SmsReceiver.kt
@@ -0,0 +1,48 @@
+package com.byandev.fundametalandroid18
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.telephony.SmsMessage
+import android.util.Log
+import java.lang.Exception
+
+class SmsReceiver : BroadcastReceiver() {
+
+    private val TAG = SmsReceiver::class.java.simpleName
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        val bundle = intent.extras
+        try {
+            if (bundle != null) {
+                val pdusObj = bundle.get("pdus") as Array<Any>
+                for (aPdusObj in pdusObj) {
+                    val currentMessage = getIncomingMessage(aPdusObj, bundle)
+                    val senderNum = currentMessage.displayOriginatingAddress
+                    val message = currentMessage.displayMessageBody
+                    Log.d(TAG, "senderNum: $senderNum; message: $message")
+                    val showSmsIntent = Intent(context, SmsReceiverActivity::class.java)
+                    showSmsIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                    showSmsIntent.putExtra(SmsReceiverActivity.EXTRA_SMS_NO,senderNum)
+                    showSmsIntent.putExtra(SmsReceiverActivity.EXTRA_SMS_MESSAGE, message)
+                    context.startActivity(showSmsIntent)
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Log.d(TAG, "Exception SmsReceicer $e")
+        }
+    }
+
+    private fun getIncomingMessage(aObject: Any, bundle: Bundle): SmsMessage {
+        val currentSMS: SmsMessage
+        if (Build.VERSION.SDK_INT >= 23) {
+            val format = bundle.getString("format")
+            currentSMS = SmsMessage.createFromPdu(aObject as ByteArray, format)
+        } else currentSMS = SmsMessage.createFromPdu(aObject as ByteArray)
+        return currentSMS
+    }
+}
