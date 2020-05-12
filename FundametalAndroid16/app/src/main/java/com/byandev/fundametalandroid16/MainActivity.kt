538fd9c47e18fbf4e633829d0ad78f6199c893e6
byansanur <ratbyansa25@gmail.com>
2020-05-12 21:16:30 +0700

    use coroutine

--- FundametalAndroid16/app/src/main/java/com/byandev/fundametalandroid16/MainActivity.kt
+++ FundametalAndroid16/app/src/main/java/com/byandev/fundametalandroid16/MainActivity.kt
@@ -1,29 +1,51 @@
 package com.byandev.fundametalandroid16
 
-import android.os.AsyncTask
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import java.lang.Exception
-import java.lang.ref.WeakReference
+import kotlinx.coroutines.*
 
-class MainActivity : AppCompatActivity(), MyAsyncCallback {
+class MainActivity : AppCompatActivity() {
 
-    // companion objek disini untuk menjalankan proses ayncTask di class mainActivity
     companion object {
+        // USE kotlin coroutine
         private const val INPUT_STRING = "Hallo ini demo AsyncTask"
+        private const val LOG_ASYNC = "DemoAsync"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        tv_status.setText(R.string.status_pre)
+        tv_desc.text = INPUT_STRING
+
+        GlobalScope.launch(Dispatchers.IO) {
+            // background thread
+            val input = INPUT_STRING
+            val output: String?
+            Log.d(LOG_ASYNC, "status : doInBackground")
+            try {
+                output = "$input Selamat Belajar!!"
+                delay(2000)
+
+                //pindah ke Main thread untuk update UI
+                withContext(Dispatchers.Main) {
+                    tv_status.setText(R.string.status_post)
+                    tv_desc.text = output
+                }
+            } catch (e: Exception) {
+                Log.d(LOG_ASYNC, e.message.toString())
+            }
+        }
+
         // pemanggilan class DemoAsync
-        val demoAsync = DemoAsync(this)
-        demoAsync.execute(INPUT_STRING)
+//        val demoAsync = DemoAsync(this)
+//        demoAsync.execute(INPUT_STRING)
     }
 
+    /*
     override fun onPreExecute() {
         tv_status.setText(R.string.status_pre)
         tv_desc.text = INPUT_STRING
@@ -33,13 +55,14 @@ class MainActivity : AppCompatActivity(), MyAsyncCallback {
         tv_status.setText(R.string.status_post)
         tv_desc.text = text
     }
+     */
 
 
 
     /*
     kelas DemoAsync yang mana kelas tersebut berfungsi untuk mengelola data secara asynchronous.
     Setelah membuat kelas DemoAsync di dalam MainActivity, kita akan membuat kelas tersebut mewarisi kelas AsyncTask
-     */
+
     private class DemoAsync(val myListener: MyAsyncCallback) : AsyncTask<String, Void, String>() {
         companion object {
             private val LOG_ASYNC = "DemoAsync"
@@ -85,11 +108,13 @@ class MainActivity : AppCompatActivity(), MyAsyncCallback {
 
 
     }
-
+    */
 
 }
 
+/*
 internal interface MyAsyncCallback {
     fun onPreExecute()
     fun onPostExecute(text: String)
 }
+ */
