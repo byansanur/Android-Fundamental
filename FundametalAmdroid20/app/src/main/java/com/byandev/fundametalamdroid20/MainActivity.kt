147d63b09726bbafe37af79aca9030af574cfaa4
byansanur <ratbyansa25@gmail.com>
2020-05-14 09:13:38 +0700

    Simple parsing json

--- FundametalAmdroid20/app/src/main/java/com/byandev/fundametalamdroid20/MainActivity.kt
+++ FundametalAmdroid20/app/src/main/java/com/byandev/fundametalamdroid20/MainActivity.kt
@@ -2,11 +2,64 @@ package com.byandev.fundametalamdroid20
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import cz.msebera.android.httpclient.Header
+import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
 
+@Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private val TAG = MainActivity::class.java
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getRandomQuote()
+    }
+
+    private fun getRandomQuote() {
+        progressBar.visibility = View.VISIBLE
+        val client = AsyncHttpClient()
+        val url = "https://programming-quotes-api.herokuapp.com/quotes/random"
+        client.get(url, object : AsyncHttpResponseHandler() {
+            override fun onSuccess(statusCode: Int, headers: Array<out Header>?, responseBody: ByteArray?) {
+                progressBar.visibility = View.INVISIBLE
+
+                val result = responseBody?.let { String(it) }
+                Log.d(TAG.toString(), result)
+                try {
+                    val responseObject = JSONObject(result)
+
+                    val quote = responseObject.getString("en")
+                    val author = responseObject.getString("author")
+
+                    tvQuote.text = quote
+                    tvAuthor.text = author
+                } catch (e: Exception) {
+                    Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_SHORT).show()
+                    e.printStackTrace()
+                }
+
+            }
+
+            override fun onFailure(statusCode: Int, headers: Array<out Header>?, responseBody: ByteArray?, error: Throwable?) {
+                progressBar.visibility = View.INVISIBLE
+                val errorMessage = when (statusCode) {
+                    401 -> "$statusCode : Bad Request"
+                    403 -> "$statusCode : Forbidden"
+                    404 -> "$statusCode : Not Found"
+                    else -> "$statusCode : ${error?.message}"
+                }
+                Toast.makeText(this@MainActivity, errorMessage, Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
