7c3a048329dddaef1febd9f60d4f2ef8c901e1e8
byansanur <ratbyansa25@gmail.com>
2020-05-15 22:19:17 +0700

    work manager sederhana

--- FundametalAndroid22/app/src/main/java/com/byandev/fundametalandroid22/MainActivity.kt
+++ FundametalAndroid22/app/src/main/java/com/byandev/fundametalandroid22/MainActivity.kt
@@ -2,11 +2,45 @@ package com.byandev.fundametalandroid22
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.work.*
+import androidx.work.Data.Builder
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnOneTimeTask.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btnOneTimeTask -> startOneTimeTask()
+        }
+    }
+
+    private fun startOneTimeTask() {
+        textStatus.text = getString(R.string.status)
+        val data = Builder()
+            .putString(MyWorker.EXTRA_CITY, editCity.text.toString())
+            .build()
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+        val oneTimeWorkRequest = OneTimeWorkRequest.Builder(MyWorker::class.java)
+            .setInputData(data)
+            .setConstraints(constraints)
+            .build()
+        WorkManager.getInstance().enqueue(oneTimeWorkRequest)
+        WorkManager.getInstance()
+            .getWorkInfoByIdLiveData(oneTimeWorkRequest.id)
+            .observe(this, androidx.lifecycle.Observer {
+                val status = it.state.name
+                textStatus.append("\n" + status)
+            })
     }
 }
---
+++ FundametalAndroid22/app/src/main/java/com/byandev/fundametalandroid22/MyWorker.kt
@@ -0,0 +1,103 @@
+package com.byandev.fundametalandroid22
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.Context.NOTIFICATION_SERVICE
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.work.ListenableWorker
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import com.loopj.android.http.AsyncHttpResponseHandler
+import com.loopj.android.http.SyncHttpClient
+import cz.msebera.android.httpclient.Header
+import org.json.JSONObject
+import java.lang.Exception
+import java.text.DecimalFormat
+
+class MyWorker(context: Context, workerParams: WorkerParameters) : Worker(context, workerParams) {
+
+    companion object {
+        private val TAG = MyWorker::class.java.simpleName
+        const val APP_ID = "fb161587654f69bd97613e2be8c32a25"
+        const val EXTRA_CITY = "city"
+        const val NOTIFICATION_ID = 1
+        const val CHANNEL_ID = "channel_01"
+        const val CHANNEL_NAME = "byandev channel"
+    }
+
+    private var resultStatus: Result? = null
+
+    override fun doWork(): Result {
+        val dataCity = inputData.getString(EXTRA_CITY)
+        val result = getCurrentWeather(dataCity)
+        return result
+    }
+
+    private fun getCurrentWeather(city: String?): Result {
+        Log.d(TAG, "getCurrentWeather: Mulai.....")
+        val client = SyncHttpClient()
+        val url = "https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$APP_ID"
+        Log.d(TAG, "getCurrentWeather: $url")
+
+        client.post(url, object : AsyncHttpResponseHandler() {
+            override fun onSuccess(
+                statusCode: Int,
+                headers: Array<out Header>,
+                responseBody: ByteArray
+            ) {
+                val result = String(responseBody)
+                Log.d(TAG, result)
+                try {
+                    val responseObject = JSONObject(result)
+                    val currentWeather: String = responseObject.getJSONArray("weather").getJSONObject(0).getString("main")
+                    val description: String = responseObject.getJSONArray("weather").getJSONObject(0).getString("description")
+                    val tempInKelvin = responseObject.getJSONObject("main").getDouble("temp")
+                    val tempInCelcius = tempInKelvin - 273
+                    val temprature: String = DecimalFormat("##.##").format(tempInCelcius)
+                    val title = "Current Weather in $city"
+                    val message = "$currentWeather, $description with $temprature celcius"
+                    showNotification(title, message)
+                    Log.d(TAG, "onSuccess: Selesai.....")
+                    resultStatus = Result.success()
+                }catch (e: Exception) {
+                    showNotification("Get Current Weather Not Success", e.message.toString())
+                    Log.d(TAG, "onSuccess: Gagal.....")
+                    resultStatus = Result.failure()
+                }
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Array<out Header>,
+                responseBody: ByteArray,
+                error: Throwable
+            ) {
+                Log.d(TAG, "onFailure: Gagal.....")
+                // ketika proses gagal, maka jobFinished diset dengan parameter true. Yang artinya job perlu di reschedule
+                showNotification("Get Current Weather Failed", error.message.toString())
+                resultStatus = Result.failure()
+            }
+
+        })
+        return resultStatus as Result
+    }
+
+    private fun showNotification(title: String, description: String) {
+        val notificationManager = applicationContext.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        val notification: NotificationCompat.Builder = NotificationCompat.Builder(applicationContext, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_notifications_black_24dp)
+            .setContentTitle(title)
+            .setContentText(description)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH)
+            notification.setChannelId(CHANNEL_ID)
+            notificationManager.createNotificationChannel(channel)
+        }
+        notificationManager.notify(NOTIFICATION_ID, notification.build())
+    }
+}
