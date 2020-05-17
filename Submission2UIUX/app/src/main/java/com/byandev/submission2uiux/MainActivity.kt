0593cff09e42333e93cb713ec7e7cd13198cb43f
byansanur <ratbyansa25@gmail.com>
2020-05-17 18:57:42 +0700

    initial commit sub2

---
+++ Submission2UIUX/app/src/main/java/com/byandev/submission2uiux/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.byandev.submission2uiux
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
