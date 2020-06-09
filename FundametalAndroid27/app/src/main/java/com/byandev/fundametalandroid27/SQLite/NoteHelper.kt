f9bd1350d17cd3bf88d6e0ef56c3a55cf9071e18
ratbyansa nur <49193857+byansanur@users.noreply.github.com>
2020-06-09 17:33:36 +0700

    simple note app with SqlLite *27

---
+++ FundametalAndroid27/app/src/main/java/com/byandev/fundametalandroid27/SQLite/NoteHelper.kt
@@ -0,0 +1,75 @@
+package com.byandev.fundametalandroid27.SQLite
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import com.byandev.fundametalandroid27.SQLite.DatabaseContract.NoteColumns.Companion.TABLE_NAME
+import com.byandev.fundametalandroid27.SQLite.DatabaseContract.NoteColumns.Companion._ID
+import java.sql.SQLException
+
+class NoteHelper(context: Context) {
+
+    companion object {
+        private const val DATABASE_TABLE = TABLE_NAME
+        private lateinit var dataBaseHelper: DatabaseHelper
+        private var INSTANCE: NoteHelper? = null
+        private lateinit var database: SQLiteDatabase
+
+        fun getInstance(context: Context): NoteHelper =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: NoteHelper(context)
+            }
+    }
+
+    init {
+        dataBaseHelper = DatabaseHelper(context)
+    }
+
+    @Throws(SQLException::class)
+    fun open() {
+        database = dataBaseHelper.writableDatabase
+    }
+    fun close() {
+        dataBaseHelper.close()
+        if (database.isOpen)
+            database.close()
+    }
+
+    fun queryAll(): Cursor {
+        return database.query(
+            DATABASE_TABLE,
+            null,
+            null,
+            null,
+            null,
+            null,
+            "$_ID ASC")
+    }
+
+    fun queryById(id: String) : Cursor {
+        return database.query(
+            DATABASE_TABLE,
+            null,
+            "$_ID = ?",
+            arrayOf(id),
+            null,
+            null,
+            null,
+            null
+        )
+
+    }
+
+    fun insert(values: ContentValues?):Long {
+        return database.insert(DATABASE_TABLE,null, values)
+    }
+
+    fun update(id: String, values: ContentValues?): Int {
+        return database.update(DATABASE_TABLE, values, "$_ID = ?", arrayOf(id))
+    }
+
+    fun deleteById(id: String): Int {
+        return database.delete(DATABASE_TABLE, "$_ID = '$id'", null)
+    }
+}
---
+++ FundametalAndroid27/app/src/main/java/com/byandev/fundametalandroid27/helper/MappingHelper.kt
@@ -0,0 +1,38 @@
+package com.byandev.fundametalandroid27.helper
+
+import android.database.Cursor
+import com.byandev.fundametalandroid27.SQLite.DatabaseContract
+import com.byandev.fundametalandroid27.entity.Note
+
+object MappingHelper {
+    fun mapCursorToArrayList(notesCursor: Cursor?): ArrayList<Note> {
+        val notesList = ArrayList<Note>()
+        notesCursor?.apply {
+            while (moveToNext()) {
+                val id = getInt(getColumnIndexOrThrow(DatabaseContract.NoteColumns._ID))
+                val title = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.TITLE))
+                val description = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.DESCRIPTION))
+                val date = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.DATE))
+                notesList.add(Note(id, title, description, date))
+            }
+        }
+        return notesList
+    }
+}
+
+/*
+    Catatan :
+    Pada NoteHelper proses load data dilakukan dengan eksekusi queryAll() menghasilkan objek Cursor,
+    namun pada adapter kita membutuhkan dalam bentuk ArrayList, maka dari itu kita harus mengonversi
+    dari Cursor ke Arraylist, di sinilah fungsi kelas pembantu MappingHelper.
+    MoveToFirst di sini digunakan untuk memindah cursor ke baris pertama sedangkan
+    MoveToNext digunakan untuk memindahkan cursor ke baris selanjutnya.
+    Di sini kita ambil datanya satu per satu dan dimasukkan ke dalam ArrayList.
+
+    //////////\\\\\\\\\
+
+    Fungsi apply digunakan untuk menyederhanakan kode yang berulang.
+    Misalnya notesCursor.geInt cukup ditulis getInt dan
+    notesCursor.getColumnIndexOrThrow cukup ditulis getColumnIndexOrThrow.
+    Untuk lengkapnya dapat anda pelajari di kelas Memulai Pemrograman Dengan Kotlin.
+ */
