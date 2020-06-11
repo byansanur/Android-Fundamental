ad1af92096a00f35d2d59cdfb8af9bc87e5f281a
ratbyansa nur <49193857+byansanur@users.noreply.github.com>
2020-06-11 20:01:20 +0700

    webChromeClient

--- FundametalAndroid31/app/src/main/java/com/byandev/fundametalandroid31/MainActivity.kt
+++ FundametalAndroid31/app/src/main/java/com/byandev/fundametalandroid31/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.byandev.fundametalandroid31
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.JsResult
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        webView.settings.javaScriptEnabled = true
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                view?.loadUrl("javascript:alert('Success load web DNA')")
+            }
+        }
+
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onJsAlert(view: WebView?, url: String?, message: String?, result: JsResult?): Boolean {
+                Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT).show()
+                result?.confirm()
+                return true
+            }
+        }
+
+        webView.loadUrl("http://digitalnetworkasia.com")
     }
 }
